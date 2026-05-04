82e1937fb2d3a4851fedc38182b5006129c9b55b
Cherly <cherlyu664@gmail.com>
2026-05-04 19:01:19 +0700

    Commit Tugas Modul 2

---
+++ Prak1_php.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Praktikum PHP</title>
+</head>
+<body>
+    <?php 
+    // Komentar pakai ini
+    echo "<h1>SELAMAT DATANG</h1>" ;
+    echo "Program PHP ku Yang Pertama";
+    /* komentar juga bisa pakai ini,
+        tapi yang ini digunkaan untuk komentar
+        barisnya lebih dari 1
+    */
+    ?>
+</body>
+</html>
